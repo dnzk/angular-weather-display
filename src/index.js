5d4dab4077533d34b0905d0e03f73ca078459b37
dnzk <danishkalesaran@gmail.com>
2016-10-12 14:32:15 +0700

    foundation of routing

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ const dependencies = [
 
 let main = angular.module('app', dependencies);
 
+main.config(function ($stateProvider, $urlRouterProvider) {
+  'ngInject';
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<h1>Home</h1>'
+    })
+})
+
